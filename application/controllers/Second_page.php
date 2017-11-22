ab976c3c9f7d96f3c10172816f18efbb63880e24
Shashidhara <shashidharkulal@gmail.com>
2017-11-22 23:43:57 +0530

    Form helper

--- application/controllers/Second_page.php
+++ application/controllers/Second_page.php
@@ -5,15 +5,36 @@ class Second_page extends CI_Controller {
 
 	public function index()
 	{
+		
+		$this->load->helper('form');
+		
 		$this->load->view('header');
 		$this->load->view('second_page');
 		$this->load->view('footer');
 	}
-	
-	public function display($param)
-	{
-		echo "Am in ".__FUNCTION__."<br />";
-		echo $param;
+		
+	public function save(){
+		
+		$this->load->library('form_validation');
+		 
+		$this->form_validation->set_rules('topic', 'Topic', 'required');
+		$this->form_validation->set_rules('date', 'Date', 'required');
+		
+		$this->load->view('header');
+		if ($this->form_validation->run() == FALSE)
+    {
+        $this->load->view('second_page');
+    }
+    else
+    {
+				$this->load->view('no_error');
+				$this->load->view('second_page');
+    }
+    
+		$this->load->view('footer');
+    
+		//echo $this->input->post("topic");
+		 //echo " ";
+		 //echo $this->input->post("date");
 	}
-	
 }
--- application/views/second_page.php
+++ application/views/second_page.php
@@ -1,9 +1,23 @@
 		<div id="body" align="center" style="width:100%">
 			<div>
-					<h3>Second page</h3>
-					<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</p>
+					<h3>TODO List</h3>
+					<?php echo validation_errors(); ?>
+					
+						<?php echo form_open('second_page/save'); ?>
+						
+						Topic:
+						<?php echo form_input('topic'); ?>
+						<br />
+						
+						Date:
+						<?php echo form_input('date'); ?>
+						<br />
+						
+						<?php echo form_submit('submit', 'Add topic'); ?>
+						<br />
+						
+						<?php echo form_close();?>
+						<br />
 			</div>
 		</div>
 
